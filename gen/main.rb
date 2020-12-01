2b2c514411f59dec76e56f7b56893bc3c0b9d37d
KrzaQ <krzaq@krzaq.cc>
2020-12-01 05:23:03 +0100

    Initial commit.

---
+++ gen/main.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+
+require 'erb'
+require 'json'
+require 'optparse'
+require 'ostruct'
+require 'time'
+
+OPTS = lambda do
+    opts = {
+        input: 'gen/data.json',
+        template: 'gen/template.erb',
+        output: 'README.md',
+    }
+
+    OptionParser.new do |o|
+        o.on('--input X'){ |x| opts[:input] = x }
+        o.on('--output X'){ |x| opts[:output] = x }
+        o.on('--template X'){ |x| opts[:template] = x }
+    end.parse!
+
+    OpenStruct.new opts
+end.call
+
+DATA = JSON.parse(File.read(OPTS.input), symbolize_names: true)
+
+t = ERB.new File.read(OPTS.template), nil, '%<>'
+
+File.write OPTS.output, t.result
