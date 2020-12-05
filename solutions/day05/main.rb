0eaf2de5e443a247dcb0c8878ef405de51a83fcb
KrzaQ <krzaq@krzaq.cc>
2020-12-05 07:19:55 +0100

    Even more simplification of Day 05.

--- solutions/day05/main.rb
+++ solutions/day05/main.rb
@@ -1,9 +1,6 @@
-def parse_seat line
-    val = line.strip.gsub(/./, %w(F B R L).zip(%w(0 1 1 0)).to_h)
-    val[0...7].to_i(2) * 8 + val[7...10].to_i(2)
-end
+to_seat = ->(l) { l.strip.gsub(/./, %w(F B R L).zip(%w(0 1 1 0)).to_h).to_i(2) }
+SEATS = File.read('data.txt').split.map{ |l| to_seat[l] }
 
-SEATS = File.read('data.txt').split.map{ |l| parse_seat l }
 PART1 = SEATS.max
 PART2 = [*SEATS.min..SEATS.max] - SEATS
 
