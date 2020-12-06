38fd463187b0f166c5cb706a8eb3bdbace215e5b
KrzaQ <krzaq@krzaq.cc>
2020-12-06 06:28:22 +0100

    Get rid of duplicate code in Day 06.

--- solutions/day06/main.rb
+++ solutions/day06/main.rb
@@ -1,6 +1,5 @@
 DATA = File.read('data.txt').split("\n\n").map(&:split)
-PART1 = DATA.map{ |g| g.map(&:chars).inject(:|).size }.sum
-PART2 = DATA.map{ |g| g.map(&:chars).inject(:&).size }.sum
+solve = ->(op) { DATA.map{ |g| g.map(&:chars).inject(op).size }.sum }
 
-puts 'Part 1: %s' % PART1
-puts 'Part 2: %s' % PART2
+puts 'Part 1: %s' % solve[:|]
+puts 'Part 2: %s' % solve[:&]
