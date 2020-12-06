3daaf0f99f10b19d5b306da5b589cbf328959ed1
KrzaQ <krzaq@krzaq.cc>
2020-12-06 23:29:58 +0100

    Save bytes.

--- solutions/day05/main.rb
+++ solutions/day05/main.rb
@@ -1,4 +1,4 @@
-to_seat = ->(l) { l.gsub(/F|L/, '0').gsub(/B|R/, '1').to_i(2) }
+to_seat = -> l { l.gsub(/F|L/, '0').gsub(/B|R/, '1').to_i(2) }
 SEATS = File.read('data.txt').split.map{ |l| to_seat[l] }
 
 PART1 = SEATS.max
--- solutions/day06/main.rb
+++ solutions/day06/main.rb
@@ -1,5 +1,5 @@
 DATA = File.read('data.txt').split("\n\n").map(&:split)
-solve = ->(op) { DATA.map{ |g| g.map(&:chars).inject(op).size }.sum }
+solve = -> op { DATA.map{ |g| g.map(&:chars).inject(op).size }.sum }
 
 puts 'Part 1: %s' % solve[:|]
 puts 'Part 2: %s' % solve[:&]
