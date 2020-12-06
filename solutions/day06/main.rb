dd03e8c7761583017f804a0799f25f5f4e8eab94
KrzaQ <krzaq@krzaq.cc>
2020-12-06 06:21:39 +0100

    Add symmetry to Day 06.

--- solutions/day06/main.rb
+++ solutions/day06/main.rb
@@ -1,5 +1,5 @@
 DATA = File.read('data.txt').split("\n\n").map(&:split)
-PART1 = DATA.map{ |g| g.join.chars.sort.uniq.size }.sum
+PART1 = DATA.map{ |g| g.map(&:chars).inject(:|).size }.sum
 PART2 = DATA.map{ |g| g.map(&:chars).inject(:&).size }.sum
 
 puts 'Part 1: %s' % PART1
