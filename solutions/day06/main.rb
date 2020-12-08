55f2e472d1748a20530cfd28d983bb06fd29988d
KrzaQ <krzaq@krzaq.cc>
2020-12-08 11:30:49 +0100

    Shave a few bytes off of Day 06. Thanks @fig.

--- solutions/day06/main.rb
+++ solutions/day06/main.rb
@@ -1,5 +1,5 @@
 DATA = File.read('data.txt').split("\n\n").map(&:split)
-solve = -> op { DATA.map{ |g| g.map(&:chars).inject(op).size }.sum }
+solve = -> op { DATA.sum{ |g| g.map(&:chars).inject(op).size } }
 
 puts 'Part 1: %s' % solve[:|]
 puts 'Part 2: %s' % solve[:&]
