e2c0d6d2bf5523fe1610f4e45e64c3a1b8ccab2a
KrzaQ <krzaq@krzaq.cc>
2020-12-09 15:21:37 +0100

    Performance enhancement for Day 09. Thx @4rk0z

--- solutions/day09/main.rb
+++ solutions/day09/main.rb
@@ -1,5 +1,5 @@
 D = File.read('data.txt').scan(/\d+/).map(&:to_i)
-P1 = D.each_cons(26).find{ |*a,b| !a.permutation(2).find{ |l, r| l+r==b } }.last
+P1 = D.each_cons(26).find{ |*a,b| !a.combination(2).find{ |l, r| l+r==b } }.last
 p2=->s,e{{1=>->{p2[s+1,e]},0=>->{[s,e]},-1=>->{p2[s,e+1]}}[D[s...e].sum<=>P1][]}
 P2 = D[Range.new(*p2[0,1])].minmax.sum
 
