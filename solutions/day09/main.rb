72ae7e0a9c9c3ef90db35a860f591b981c03758d
KrzaQ <krzaq@krzaq.cc>
2020-12-09 07:43:52 +0100

    Golf Day 09 a bit.

--- solutions/day09/main.rb
+++ solutions/day09/main.rb
@@ -1,17 +1,14 @@
 DATA = File.read('data.txt').scan(/\d+/).map(&:to_i)
 
-PART1 = DATA.each_cons(26).find do |a|
-    r = a[0...25].product(a[0...25]).find do |l, r|
-        l != r and a.last == l + r
-    end
-    not r
+PART1 = DATA.each_cons(26).find do |*a, b|
+    not a.permutation(2).find{ |l, r| l + r == b }
 end.last
 
-PART2 = (2...DATA.find_index(PART1)).map do |n|
-    DATA[0...DATA.find_index(PART1)].each_cons(n).find do |a|
-        a.sum == PART1
-    end
-end.select(&:itself).first.minmax.sum
+def p2 s = 0, e = 1
+    r = DATA[s...e].sum
+    r < PART1 ? p2(s, e+1) : r > PART1 ? p2(s+1, e) : [s, e]
+end
+PART2 = DATA[Range.new(*p2)].minmax.sum
 
 puts 'Part 1: %s' % PART1
 puts 'Part 2: %s' % PART2
