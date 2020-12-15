bbc63ec10e187f2bd2cf46a6648134c5d8554703
KrzaQ <krzaq@krzaq.cc>
2020-12-15 07:38:46 +0100

    Shorten Day 11 even more. Also thanks @havenwood!

--- solutions/day11/main.rb
+++ solutions/day11/main.rb
@@ -2,9 +2,9 @@ D = File.read('data.txt').lines.map(&:strip); W = D[0].size; H = D.size;
 DS = [-1,0,1].repeated_permutation(2).to_a - [[0,0]]
 legit = ->x,y{x>=0&&y>=0&&x<W&&y<H}; s = ->a,b{a.zip(b).map(&:sum)}
 fns = ->d,x,y{x,y=s[d,[x,y]];legit[x,y] ?D[y][x]==?L? [x,y]: fns[d,x,y]:nil}
-S = [*0...W].product([*0...H]).map{|x,y|[[x,y],DS.filter_map{|d|fns[d,x,y]}]}.to_h
+S = [*0...W].product([*0...H]).map{|x,y|[[x,y],DS.filter_map{fns[_1,x,y]}]}.to_h
 a = ->x,y{DS.zip([[x,y]]*8).filter_map{|a|legit[*s[*a]]&&s[*a]}}
-o = ->d,x,y{a[x,y].count{|x,y|d[y][x]==?#}};v = ->d,x,y{S[[x,y]].count{|x,y|d[y][x]==?#}}
+o = ->d,x,y{a[x,y].count{d[_2][_1]==?#}};v = ->d,x,y{S[[x,y]].count{d[_2][_1]==?#}}
 iter = ->i,f{r=i.map &:dup;0.upto(W-1){|x|0.upto(H-1){|y|r[y][x]=f[i,x,y]}};r}
 hlp = ->o,m{{?.=>->_{?.},?L=>->o{o==0??#:?L},?#=>->o{o>=m ? ?L:?#}}[o]}
 fp = ->n,m{->d,x,y{hlp[d[y][x],n][m[d,x,y]]}}; fp1 = fp[4,o]; fp2 = fp[5,v]
