892fb00b57dd37c387057097fdb5f88f75987ca0
KrzaQ <krzaq@krzaq.cc>
2020-12-25 06:55:23 +0100

    Too long is too long. Day 25.

--- solutions/day25/main.rb
+++ solutions/day25/main.rb
@@ -1,15 +1,5 @@
 PK1, PK2 = File.read('data.txt').split.map(&:to_i)
 MOD = 20201227
-n1, n2 = [nil, nil]
-
-(1..).each do |n|
-    v = 7.pow(n, MOD)
-    if v == PK1
-        n1 = n
-    elsif v == PK2 
-        n2 = n
-    end
-    break if n1 and n2
-end
-
-puts 'Part 1: %s' % PK1.pow(n2, MOD)
+PART1 = (1..).each.find{ v = 7.pow(_1, MOD); v == PK1 or v == PK2 }
+    .yield_self{ (7.pow(_1, MOD) == PK1 ? PK2 : PK1).pow(_1, MOD) }
+puts 'Part 1: %s' % PART1
